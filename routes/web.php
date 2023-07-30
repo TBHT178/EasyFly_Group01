f00169b5abbbe923ee2dc7ee519f2601a0b26b40
ngothinhphat <ngothinhphat@192.168.2.15>
2023-07-30 17:27:21 +0700

    cap nhat index

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\Auth\RegisterController;
@@ -8,11 +9,11 @@ use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\AdminController;
 
 
-Route::get('/', [ProjectController::class, 'index']) ->name('index');
-Route::get('/about', [ProjectController::class, 'about']) ->name('about');
-Route::get('/contact', [ProjectController::class, 'contact']) ->name('contact');
-Route::get('/blog-details', [ProjectController::class, 'blogdetails']) ->name('blog-details');
-Route::get('/blog', [ProjectController::class, 'blog']) ->name('blog');
+Route::get('/', [ProjectController::class, 'index'])->name('index');
+Route::get('/about', [ProjectController::class, 'about'])->name('about');
+Route::get('/contact', [ProjectController::class, 'contact'])->name('contact');
+Route::get('/blog-details', [ProjectController::class, 'blogdetails'])->name('blog-details');
+Route::get('/blog', [ProjectController::class, 'blog'])->name('blog');
 
 Route::get('register', [RegisterController::class, 'index']);
 Route::post('register', [RegisterController::class, 'store'])->name('register');
@@ -29,47 +30,61 @@ Route::post('forget-password', [ForgotPasswordController::class, 'postEmail'])->
 
 Route::get('reset-password/{token}', [ResetPasswordController::class, 'getPassword'])->name('password.reset');
 Route::post('reset-password', [ResetPasswordController::class, 'updatePassword'])->name('password.update');
-Route::get('/admin',[AdminController::class,'dashboard'])->name('dashboard');
+Route::get('/admin', [AdminController::class, 'dashboard'])->name('dashboard');
 #Admin
 
-Route::prefix('/admin/airport')->group(function(){
+Route::prefix('/admin/airport')->group(function () {
     #airport
-    Route::get('/',[AdminController::class,'sanbay'])->name('sanbay');
-    Route::post('/search',[AdminController::class,'searchairport'])->name('searchairport');
+    Route::get('/', [AdminController::class, 'sanbay'])->name('sanbay');
+    Route::post('/search', [AdminController::class, 'searchairport'])->name('searchairport');
     #them
-    Route::get('/add',[AdminController::class,'sb_add']) ->name('sb_add');
-    Route::post('/addprocess',[AdminController::class,'sb_addprocess']) ->name('sb_addprocess');
+    Route::get('/add', [AdminController::class, 'sb_add'])->name('sb_add');
+    Route::post('/addprocess', [AdminController::class, 'sb_addprocess'])->name('sb_addprocess');
     #update
-    Route::get('/update/{code}',[AdminController::class,'sb_update'])->name('sb_update');
-    Route::post('/updateprocess/{code}',[AdminController::class,'sb_updateprocess'])->name('sb_updateprocess');
+    Route::get('/update/{code}', [AdminController::class, 'sb_update'])->name('sb_update');
+    Route::post('/updateprocess/{code}', [AdminController::class, 'sb_updateprocess'])->name('sb_updateprocess');
     #delete
-    Route::get('/delete/{code}',[AdminController::class,'sb_delete'])->name('sb_delete');
+    Route::get('/delete/{code}', [AdminController::class, 'sb_delete'])->name('sb_delete');
 });
 
-Route::prefix('/admin/flight')->group(function(){
+Route::prefix('/admin/flight')->group(function () {
     #flight
-    Route::get('/',[AdminController::class,'flight'])->name('flight');
-    Route::post('/search',[AdminController::class,'searchflight'])->name('searchflight');
+    Route::get('/', [AdminController::class, 'flight'])->name('flight');
+    Route::post('/search', [AdminController::class, 'searchflight'])->name('searchflight');
     #them
-    Route::get('/add',[AdminController::class,'flight_add']) ->name('flight_add');
-    Route::post('/addprocess',[AdminController::class,'flight_addprocess']) ->name('flight_addprocess');
+    Route::get('/add', [AdminController::class, 'flight_add'])->name('flight_add');
+    Route::post('/addprocess', [AdminController::class, 'flight_addprocess'])->name('flight_addprocess');
     #update
-    Route::get('/update/{code}',[AdminController::class,'flight_update'])->name('flight_update');
-    Route::post('/updateprocess/{code}',[AdminController::class,'flight_updateprocess'])->name('flight_updateprocess');
+    Route::get('/update/{code}', [AdminController::class, 'flight_update'])->name('flight_update');
+    Route::post('/updateprocess/{code}', [AdminController::class, 'flight_updateprocess'])->name('flight_updateprocess');
     #delete
-    Route::get('/delete/{code}',[AdminController::class,'flight_delete'])->name('flight_delete');
+    Route::get('/delete/{code}', [AdminController::class, 'flight_delete'])->name('flight_delete');
 });
 
-Route::prefix('/admin/feedback')->group(function(){
+Route::prefix('/admin/feedback')->group(function () {
     #flight
-    Route::get('/',[AdminController::class,'feedback'])->name('feedback');
-    Route::post('/search',[AdminController::class,'searchfeedback'])->name('searchfeedback');
+    Route::get('/', [AdminController::class, 'feedback'])->name('feedback');
+    Route::post('/search', [AdminController::class, 'searchfeedback'])->name('searchfeedback');
     #them
-    Route::get('/add',[AdminController::class,'feedback_add']) ->name('feedback_add');
-    Route::post('/addprocess',[AdminController::class,'feedback_addprocess']) ->name('feedback_addprocess');
+    Route::get('/add', [AdminController::class, 'feedback_add'])->name('feedback_add');
+    Route::post('/addprocess', [AdminController::class, 'feedback_addprocess'])->name('feedback_addprocess');
     #update
-    Route::get('/update/{code}',[AdminController::class,'feedback_update'])->name('feedback_update');
-    Route::post('/updateprocess/{code}',[AdminController::class,'feedback_updateprocess'])->name('feedback_updateprocess');
+    Route::get('/update/{code}', [AdminController::class, 'feedback_update'])->name('feedback_update');
+    Route::post('/updateprocess/{code}', [AdminController::class, 'feedback_updateprocess'])->name('feedback_updateprocess');
     #delete
-    Route::get('/delete/{code}',[AdminController::class,'feedback_delete'])->name('feedback_delete');
+    Route::get('/delete/{code}', [AdminController::class, 'feedback_delete'])->name('feedback_delete');
+});
+// seat_class
+Route::prefix('/admin/seatclass')->group(function () {
+    #seatclass
+    Route::get('/', [AdminController::class, 'seatclass'])->name('seatclass');
+    Route::post('/search', [AdminController::class, 'searchseatclass'])->name('searchseatclass');
+    #them
+    Route::get('/add', [AdminController::class, 'seatclass_add'])->name('seatclass_add');
+    Route::post('/addprocess', [AdminController::class, 'seatclass_addprocess'])->name('seatclass_addprocess');
+    #update
+    Route::get('/update/{code}', [AdminController::class, 'seatclass_update'])->name('seatclass_update');
+    Route::post('/updateprocess/{code}', [AdminController::class, 'seatclass_updateprocess'])->name('seatclass_updateprocess');
+    #delete
+    Route::get('/delete/{code}', [AdminController::class, 'seatclass_delete'])->name('seatclass_delete');
 });
